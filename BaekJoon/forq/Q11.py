c9d33b5eb647e957d1dc623fed40903cd7996798
inthesnow <qlsdl16@gmail.com>
2021-11-01 22:20:06 +0900

    clear forq

--- BaekJoon/forq/Q11.py
+++ BaekJoon/forq/Q11.py
@@ -1,6 +1,14 @@
 import sys
 
-n, x = int(sys.stdin.readline().split())
-a = [int(sys.stdin.readline().split)]
+n = sys.stdin.readline().split()
+n = list(map(int, n))
+a = sys.stdin.readline().split()
+a = list(map(int, a))
 
-print(a)
+x=""
+
+for i in range(n[0]) :
+    if a[i] < n[1] :
+        x +=str(a[i])+" "
+
+print(x.rstrip())
