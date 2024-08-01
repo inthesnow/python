97a1e68cdf875694128029a45132ad18d6052a17
inthesnow <qlsdl16@gmail.com>
2024-08-01 17:55:55 +0900

    8/1

--- JompToPython/sample.py
+++ JompToPython/sample.py
@@ -4,5 +4,10 @@ n, m = map(int, sys.stdin.readline().split())
 b = list(range(1,n+1))
 for i in range(m):
     x, y = map(int, sys.stdin.readline().split())
-    for x in y:
-        print(i)
+    tl=list(range(x,y))
+    print(tl)
+    for j in range(x,y):
+        tmp=tl[j-1]
+        tl[j-1]=tl[y-j]
+        tl[y-j]=tmp
+    print(tl)
